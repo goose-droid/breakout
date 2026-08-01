57c5484bf3947c80516086157d543582ddd2a5f3
Ollie Christensen <ollie.a.c@outlook.com>
2026-08-01 09:19:57 -0600

    second step done

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
+//constants from html
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
+//ball starting position
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+//change in x and change in y
+//how much we want ball to move on every draw()
+var dx = -2;
+var dy = -1;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgb(0 0 255 / 50%)";
-ctx.stroke();
-ctx.closePath();
+//function to draw the ball
+function drawBall() {
+    ctx.beginPath();
+    //(position x, position y, radius, 
+    // starting angle (radians), ending angle (radians))
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+//main function that will draw every 10 ms
+function draw() {
+    //clear canvas
+    //(coordinates of top left, coordinates of bottom right)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //draw ball
+    drawBall();
+
+    //change ball position using dx and dy
+    x += dx;
+    y += dy;
+}
+
+//call draw() every 20 ms
+setInterval(draw, 10);
